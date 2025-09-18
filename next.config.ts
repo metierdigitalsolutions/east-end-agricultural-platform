15ddf4dc52a63e5920320c739beda8270862325e
Femi Ajao <phemorah@gmail.com>
2025-09-18 08:07:09 +0100

    Fixed base path issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/beta', // Add this to prefix all routes and assets with /beta
   images: {
     unoptimized: true, // Required for static export
     remotePatterns: [
